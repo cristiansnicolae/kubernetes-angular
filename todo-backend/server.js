e7c86339710b60a569df307deda1d26c0d8b4936
cris <cris@pop-os.localdomain>
2023-08-25 10:14:56 +0300

    Initial commit

---
+++ todo-backend/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://mongo.default.svc.cluster.local:27017/todo', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const taskSchema = new mongoose.Schema({
+  task: String,
+});
+
+const Task = mongoose.model('Task', taskSchema);
+
+app.get('/tasks', async (req, res) => {
+  const tasks = await Task.find();
+  res.json(tasks);
+});
+
+app.post('/tasks', async (req, res) => {
+  const task = new Task(req.body);
+  await task.save();
+  res.json(task);
+});
+
+const PORT = 3000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
+});
